afc584068eba8934a11d9f62f4e2828ab2daff99
Lewis Clements <lewisclements15@icloud.com>
2024-03-01 07:02:58 +0000

    add code to make the game more user friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+})
     runGame("addition");
 })
 
@@ -20,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 // Creates 2 random numbers between 1 and 25
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1; 
@@ -95,13 +104,15 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "-";
 }
 
